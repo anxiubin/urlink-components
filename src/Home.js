0401926f99b4480a042da52596e7496bab606f7d
anxiubin <tnqls0120@naver.com>
2020-04-09 09:19:49 +0900

    ENH: set Router and Link

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import URLinkLogo from './Img/logo-urlink-full.png'
+
+function Home() {
+  return (
+    <>
+    <img className="logo-URLink" alt="URLink" src={URLinkLogo} style={{margin: 20, width: 155, height: 35}} />
+    <div style={{margin: 10}}>
+        <Button variant="contained" color="primary">
+            <Link to="/Login" style={{color:"#fff", textDecorationLine: "none"}}>Login</Link>
+        </Button>
+    </div>
+    <div style={{margin: 10}}>
+        <Button variant="contained" color="primary">
+            <Link to="/Signup" style={{color:"#fff", textDecorationLine: "none"}}>Signup</Link>
+        </Button>  
+    </div>
+    </>
+  );
+}
+export default Home;
